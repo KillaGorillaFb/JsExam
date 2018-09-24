c7d2bbaa0f3d5324164252de326bd0373b351911
[Frankie Barrios] <[softwaredevfb@gmail.com]>
2018-09-24 08:05:47 -0500

    Cleaned up Code

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-/*
-Developer:  Frankie Barrios 
-Date:       9/20/2018
-Purpose:    Exam 1: Javascript
-*/
 const cart = {
     firstName: "Lou",
     lastName: "Tennant",
     cartItems: ["Rubber Duck", "Stitch Erase"],
     itemPrices: [15.95, 1.95],
     printShoppingCart() {
-        inCart = 0;
         for (var i = 0; i < this.cartItems.length - 1; i++) {
-            inCart += this.cartItems[i];
             console.log(`Name: ${this.firstName} ${this.lastName}
             \rItems Purchased:\n${this.cartItems[0]} $${this.itemPrices[0]} 
             \r${this.cartItems[1]} $${this.itemPrices[1]}`);
@@ -24,5 +17,4 @@ const cart = {
         console.log(`Total Purchase: $${finalTotal}`);
     }
 }
-
 cart.printShoppingCart();
